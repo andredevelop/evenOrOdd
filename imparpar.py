414cfbdd2d2e6d2bf3bd946117a4be5cd599c116
Francisco André <73521282+andredevelop@users.noreply.github.com>
2023-06-23 20:36:12 -0300

    import

---
+++ imparpar.py
@@ -0,0 +1,10 @@
+"""
+descobrir se é impar ou par
+"""
+
+numero = int(input('Insira o número para saber se é impar ou par: '))
+
+if (numero % 2 == 0):
+    print(f"{numero} É PAR")
+else:
+    print(f"{numero} É IMPAR")
